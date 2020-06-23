f02d77a55e181cabf0611463a0709e928a244783
deyteguh <dedyteguhs@gmail.com>
2020-06-23 10:44:16 +0700

    Tutorial Membuat CRUD dengan API

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UserSeeder::class);
+        $faker = Faker\Factory::create(); //import library faker
+
+        $limit = 20; //batasan berapa banyak data
+
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('kontaks')->insert([ //mengisi datadi database
+                'nama' => $faker->name,
+                'email' => $faker->unique()->email, //email unique sehingga tidak ada yang sama
+                'nohp' => $faker->phoneNumber,
+                'alamat' => $faker->address,
+            ]);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/kontak','KontakController@index');
+Route::get('/kontak/{id}','KontakController@show');
+
+Route::post('/kontak/store','KontakController@store');
+Route::post('/kontak/update/{id}','KontakController@update');
+Route::post('/kontak/delete/{id}','KontakController@destroy');
